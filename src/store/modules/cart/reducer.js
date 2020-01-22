394eb5865150f97539256676de77acf4937f1047
André Borges <hoilut@hotmail.com>
2020-01-21 23:19:26 -0300

    Update - Rocketshoes

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,35 @@
-export default function cart() {
-  return [];
+import produce from 'immer';
+
+export default function cart(state = [], action) {
+  // Só cai no reducer se a action for 'ADD_TO_CART'
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
+    default:
+      return state;
+  }
 }
+
+/*
+Sem o immer
+
+return [
+  ...state,
+  {
+    ...action.product,
+    amount: 1,
+  },
+]; 
+
+*/
